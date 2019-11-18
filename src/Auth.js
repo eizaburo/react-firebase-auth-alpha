68bd6705a53fb79225ef0b07384bbeac996068de
eizaburo <eizaburo.tamaki@gmail.com>
2019-11-18 10:59:39 +0900

    いちおう動くが、update on an unmounted compnent warningが出る

--- src/Auth.js
+++ src/Auth.js
@@ -5,6 +5,7 @@ import firebase from './Firebase';
 class Auth extends React.Component {
 
     state = {
+        signInCheck: false,
         signedIn: false,
     }
 
@@ -12,11 +13,22 @@ class Auth extends React.Component {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
                 this.setState({ signedIn: true })
+                this.setState({ signInCheck: true })
+            } else {
+                this.setState({ signedIn: false })
+                this.setState({ signInCheck: true })
             }
         });
     }
 
     render() {
+
+        //ログイン状態チェックが完了するまで待つ
+        if(!this.state.signInCheck){
+            return <p>ログインチェック中...</p>
+        }
+
+        //ログイン状態により振り分け
         if (this.state.signedIn) {
             return this.props.children;
         } else {
